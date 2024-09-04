83cb38854ccd53a6cf157905d920634ee5f3f65e
JaiDixit <dixitjai1584@gmail.com>
2024-09-04 21:56:57 +0530

    for deployment2

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ predict_pipeline = PredictPipeline()
 if st.button("Predict"):
     prediction = predict_pipeline.predict(input_df)
     st.subheader("Predicted Price ")
-    st.write(f"**{prediction[0]:.2f} LAKH RUPEES **")
+    st.write(f"** ₹ {prediction[0]:.2f} LAKH RUPEES **")
 
 
 # Footer with author's name
